f6a74870d0f510d4e6dec0e00dddd7f3cfe4eba9
ysalomon <yamila.salomon@enerminds.com>
2016-07-04 15:05:44 -0300

    plan

--- viz_mapa_integracion_batch_certa/conf/syncCertaHistoricPlan.d/60red.js
+++ viz_mapa_integracion_batch_certa/conf/syncCertaHistoricPlan.d/60red.js
@@ -1,12 +1,10 @@
 syncPlanAgregarLista([
-//"categoria_h.new",
+	"red_h.new",
+	"categoria_h.new",
+	"atr_def_h.new",
+	"clase_h.new",
+	"elemento_h.new"
 //"users_h.new",
-//"red_h.new",
-//"clase_h.new",
-"elemento_h.new",
-//"atr_def_h.new",
-"topologia_h.new",
-"elem_atr_h.new"
 //"GeoHub.new",
 //"GeoSitios.new"
 ]);
